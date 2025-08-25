7122b5460a181c333b0d9de48e0dbad263db8df0
ArKa <91108604+Arue434@users.noreply.github.com>
2025-08-25 14:21:51 +0700

    array nama 3 orang dengan nilainya

---
+++ arrnef.js
@@ -0,0 +1,10 @@
+let nameu = ["dd", "de", "da"];
+console.log("namanya : " + nameu);
+
+let nilai = [90, 80, 70, 60, 50];
+console.log("nilainya : " + nilai);
+
+for (let i = 0; i < nameu.length; i++) {
+    console.log("nama" + " : " + nameu[i] + " nilai : " + nilai[i]);
+}
+
